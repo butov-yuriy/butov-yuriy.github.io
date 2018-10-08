f3301350675c749dca4c365436313c4c56874e78
butov-yuriy <43738133+butov-yuriy@users.noreply.github.com>
2018-10-08 19:48:17 +0600

    Не законченная верстка сайта TazaHome
    Загрузил для проверки

---
+++ TazaHome/js/main.js
@@ -0,0 +1,9 @@
+$('.services-slider').slick({
+  infinite: false,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  arrows: true,
+  dots: true,
+  prevArrow: '<i class="slick-btn left-btn fa fa-chevron-left" aria-hidden="true"></i>',
+  nextArrow: '<i class="slick-btn right-btn fa fa-chevron-right" aria-hidden="true"></i>',
+});
