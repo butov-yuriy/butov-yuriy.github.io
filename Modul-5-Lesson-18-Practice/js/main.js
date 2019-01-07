f9bef5882fc4b104c555ce3f64f7b3198cb477c1
butov-yuriy <43738133+butov-yuriy@users.noreply.github.com>
2019-01-07 11:21:53 +0600

    Шестая часть практики (блок с ценами)

---
+++ Modul-5-Lesson-18-Practice/js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+  $('.features-slider').slick({
+  	infinite: true,
+  	slidesToShow: 4,
+  	slidesToScroll: 1,
+   responsive: [
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        prevArrow: '<button class="arrow prev"></button>',
+        nextArrow: '<button class="arrow next"></button>'
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button class="arrow prev"></button>',
+        nextArrow: '<button class="arrow next"></button>'
+      }
+    }
+   ]
+  });
+  $('.popup-btn').on('click', function(event) {
+    event.preventDefault();
+    $('.popup').fadeIn();
+  });
+  $('.popup-close').on('click', function(event) {
+    event.preventDefault();
+    $('.popup').fadeOut();
+  });
+});
+
