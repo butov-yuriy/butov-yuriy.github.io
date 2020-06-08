3bf44f9c491efcf12a0b3b15d99f9f4f689497bf
butov-yuriy <43738133+butov-yuriy@users.noreply.github.com>
2020-06-08 12:03:58 +0600

    Add files via upload

--- TazaHome/js/main.js
+++ TazaHome/js/main.js
@@ -1,19 +1,10 @@
-$('.services-slider').slick({
-  infinite: false,
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  speed: 100,
-  arrows: true,
-  dots: true,
-  prevArrow: '<i class="slick-btn left-btn fa fa-chevron-left" aria-hidden="true"></i>',
-  nextArrow: '<i class="slick-btn right-btn fa fa-chevron-right" aria-hidden="true"></i>',
-  responsive:[{
-  	breakpoint: 768,
-      settings: {
-    		slidesToShow: 1,
-    	}
-  }
-  ]
+$('.feedback-items').slick({
+	dots: true,
+	arrows: false,
+	infinite: false,
+	speed: 1000,
+	autoplay: true,
+	pauseOnHover: true,
+	pauseOnDotsHover: true,
+	autoplaySpeed: 7000,
 });
-
-
